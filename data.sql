dd4761c18818e9b8d2ff4cb537ae2204dbc48c1e
ZHChu98 <845655005@qq.com>
2021-05-05 20:42:02 +0800

    model

---
+++ data.sql
@@ -0,0 +1,218 @@
+-- ydzc_auth
+INSERT INTO ydzc_auth VALUES(NULL, 'Zeke', 'Yeager', 'zeke@nyu.edu', '3#', 'West Road', 'NewYork', 'NY', 'US', 10001);
+INSERT INTO ydzc_auth VALUES(NULL, 'Bertolt', 'Hoover', 'bertolt@nyu.edu', '81#', 'North Road', 'Phoenix', 'AZ', 'US', 10021);
+INSERT INTO ydzc_auth VALUES(NULL, 'Annie', 'Leonhart', 'annie@nyu.edu', '21#', 'South Road', 'Montgomery', 'AL', 'US', 10081);
+INSERT INTO ydzc_auth VALUES(NULL, 'Marcel', 'Galliard', 'marcel@nyu.edu', '33#', 'West Road', 'Denver', 'CO', 'US', 10011);
+INSERT INTO ydzc_auth VALUES(NULL, 'Porco', 'Galliard', 'porco@nyu.edu', '22#', 'West Road', 'Los Angeles', 'CA', 'US', 10026);
+INSERT INTO ydzc_auth VALUES(NULL, 'Pieck', 'Finger', 'pieck@nyu.edu', '88#', 'West Road', 'Chicago', 'IL', 'US', 10024);
+INSERT INTO ydzc_auth VALUES(NULL, 'Gabi', 'Braun', 'gabi@nyu.edu', '72#', 'West Road', 'Boston', 'MA', 'US', 10052);
+INSERT INTO ydzc_auth VALUES(NULL, 'Falco', 'Grice', 'falco@nyu.edu', '90#', 'West Road', 'Detroit', 'MI', 'US', 10073);
+-- ydzc_bk
+INSERT INTO ydzc_bk VALUES (9782222211111, 'Western Culture');
+INSERT INTO ydzc_bk VALUES (9783333322222, 'Le Petit Prince');
+INSERT INTO ydzc_bk VALUES (9784444433333, 'Van Gogh: The Life');
+INSERT INTO ydzc_bk VALUES (9785555544444, 'Lonely Planet');
+INSERT INTO ydzc_bk VALUES (9786666655555, 'The Beauty of Science');
+INSERT INTO ydzc_bk VALUES (9787777766666, 'Little Duck');
+INSERT INTO ydzc_bk VALUES (9788888877777, 'Hundred Thousand Whys');
+INSERT INTO ydzc_bk VALUES (9789999988888, 'Ancient Egypt');
+-- ydzc_bk_auth
+INSERT INTO ydzc_bk_auth VALUES (1, 9782222211111);
+INSERT INTO ydzc_bk_auth VALUES (1, 9783333322222);
+INSERT INTO ydzc_bk_auth VALUES (2, 9783333322222);
+INSERT INTO ydzc_bk_auth VALUES (3, 9784444433333);
+INSERT INTO ydzc_bk_auth VALUES (4, 9785555544444);
+INSERT INTO ydzc_bk_auth VALUES (5, 9786666655555);
+INSERT INTO ydzc_bk_auth VALUES (6, 9787777766666);
+INSERT INTO ydzc_bk_auth VALUES (7, 9788888877777);
+INSERT INTO ydzc_bk_auth VALUES (8, 9788888877777);
+INSERT INTO ydzc_bk_auth VALUES (8, 9789999988888);
+-- ydzc_top
+INSERT INTO ydzc_top VALUES ('History');
+INSERT INTO ydzc_top VALUES ('Children');
+INSERT INTO ydzc_top VALUES ('Science');
+INSERT INTO ydzc_top VALUES ('Arts');
+INSERT INTO ydzc_top VALUES ('Travel');
+-- ydzc_bk_top
+INSERT INTO ydzc_bk_top VALUES (9782222211111, 'Arts');
+INSERT INTO ydzc_bk_top VALUES (9784444433333, 'Arts');
+INSERT INTO ydzc_bk_top VALUES (9789999988888, 'Arts');
+INSERT INTO ydzc_bk_top VALUES (9783333322222, 'Children');
+INSERT INTO ydzc_bk_top VALUES (9786666655555, 'Children');
+INSERT INTO ydzc_bk_top VALUES (9787777766666, 'Children');
+INSERT INTO ydzc_bk_top VALUES (9788888877777, 'Children');
+INSERT INTO ydzc_bk_top VALUES (9786666655555, 'History');
+INSERT INTO ydzc_bk_top VALUES (9788888877777, 'History');
+INSERT INTO ydzc_bk_top VALUES (9784444433333, 'Science');
+INSERT INTO ydzc_bk_top VALUES (9789999988888, 'Science');
+INSERT INTO ydzc_bk_top VALUES (9785555544444, 'Travel');
+-- ydzc_bkcpy
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'N', 9782222211111);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'Y', 9782222211111);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'Y', 9782222211111);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'N', 9783333322222);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'N', 9784444433333);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'Y',9785555544444);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'Y', 9786666655555);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'Y', 9787777766666);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'Y', 9788888877777);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'Y', 9788888877777);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'Y', 9788888877777);
+INSERT INTO ydzc_bkcpy VALUES (NULL, 'Y', 9789999988888);
+-- ydzc_cust
+INSERT INTO ydzc_cust VALUES (NULL, 'Eren', 'Yeager', 2129981211, 'eren@nyu.edu', 'S', 437625001);
+INSERT INTO ydzc_cust  VALUES (NULL, 'Mikasa', 'Ackerman', 2129981212, 'mikasa@nyu.edu', 'D', 437625002);
+INSERT INTO ydzc_cust VALUES (NULL, 'Armin', 'Arlert', 2129981213, 'armin@nyu.edu', 'S', 437625003);
+INSERT INTO ydzc_cust VALUES (NULL, 'Reiner', 'Braun', 2129981214, 'reiner@nyu.edu', 'P', 437625004);
+INSERT INTO ydzc_cust VALUES (NULL, 'Jean', 'Kirstein', 2129981215, 'jean@nyu.edu', 'D', 437625005);
+INSERT INTO ydzc_cust VALUES (NULL, 'Marco', 'Bott', 2129981216, 'marco@nyu.edu', 'S', 437625006);
+INSERT INTO ydzc_cust VALUES (NULL, 'Connie', 'Springer', 2129981217, 'connie@nyu.edu', 'P', 437625007);
+INSERT INTO ydzc_cust VALUES (NULL, 'Sasha', 'Blouse', 2129981218, 'sasha@nyu.edu', 'P', 437625008);
+INSERT INTO ydzc_cust VALUES (NULL, 'Historia', 'Reiss', 2129981219, 'historia@nyu.edu', 'D', 437625009);
+-- ydzc_rent
+INSERT INTO ydzc_rent VALUES (10000001, 'R', STR_TO_DATE('01/02/2021', '%d/%m/%Y'), STR_TO_DATE('08/02/2021', '%d/%m/%Y'), STR_TO_DATE('08/02/2021', '%d/%m/%Y'), 4, 3);
+INSERT INTO ydzc_rent VALUES (10000002, 'R', STR_TO_DATE('01/02/2021', '%d/%m/%Y'), STR_TO_DATE('15/02/2021', '%d/%m/%Y'), STR_TO_DATE('06/02/2021', '%d/%m/%Y'), 6, 2);
+INSERT INTO ydzc_rent VALUES (10000003, 'R', STR_TO_DATE('01/02/2021', '%d/%m/%Y'), STR_TO_DATE('15/02/2021', '%d/%m/%Y'), STR_TO_DATE('11/02/2021', '%d/%m/%Y'), 8, 1);
+INSERT INTO ydzc_rent VALUES (10000004, 'L', STR_TO_DATE('01/02/2021', '%d/%m/%Y'), STR_TO_DATE('15/02/2021', '%d/%m/%Y'), STR_TO_DATE('18/02/2021', '%d/%m/%Y'), 11, 5);
+INSERT INTO ydzc_rent VALUES (10000005, 'L', STR_TO_DATE('01/02/2021', '%d/%m/%Y'), STR_TO_DATE('15/02/2021', '%d/%m/%Y'), STR_TO_DATE('18/02/2021', '%d/%m/%Y'), 12, 5);
+INSERT INTO ydzc_rent VALUES (10000006, 'B', STR_TO_DATE('01/04/2021', '%d/%m/%Y'), STR_TO_DATE('01/05/2021', '%d/%m/%Y'), NULL, 1, 6);
+INSERT INTO ydzc_rent VALUES (10000007, 'B', STR_TO_DATE('01/04/2021', '%d/%m/%Y'), STR_TO_DATE('01/05/2021', '%d/%m/%Y'), NULL, 4, 6);
+INSERT INTO ydzc_rent VALUES (10000008, 'B', STR_TO_DATE('01/04/2021', '%d/%m/%Y'), STR_TO_DATE('01/05/2021', '%d/%m/%Y'), NULL, 5, 6);
+-- ydzc_invc
+INSERT INTO ydzc_invc VALUES (20000001, STR_TO_DATE('06/02/2021', '%d/%m/%Y'), 1.00, 10000002);
+INSERT INTO ydzc_invc VALUES (20000002, STR_TO_DATE('08/02/2021', '%d/%m/%Y'), 1.40, 10000001);
+INSERT INTO ydzc_invc VALUES (20000003, STR_TO_DATE('11/02/2021', '%d/%m/%Y'), 2.00, 10000003);
+INSERT INTO ydzc_invc VALUES (20000004, STR_TO_DATE('18/02/2021', '%d/%m/%Y'), 4.00, 10000004);
+INSERT INTO ydzc_invc VALUES (20000005, STR_TO_DATE('18/02/2021', '%d/%m/%Y'), 4.00, 10000005);
+-- ydzc_pay
+INSERT INTO ydzc_pay VALUES (30000001, STR_TO_DATE('06/02/2021', '%d/%m/%Y'), 1.00, 'PAYPAL', 10000002, 20000001);
+INSERT INTO ydzc_pay VALUES (30000002, STR_TO_DATE('08/02/2021', '%d/%m/%Y'), 1.40, 'CREDIT', 10000001, 20000002);
+INSERT INTO ydzc_pay VALUES (30000003, STR_TO_DATE('12/02/2021', '%d/%m/%Y'), 1.00, 'CREDIT', 10000003, 20000003);
+INSERT INTO ydzc_pay VALUES (30000004, STR_TO_DATE('14/02/2021', '%d/%m/%Y'), 0.50, 'CREDIT', 10000003, 20000003);
+INSERT INTO ydzc_pay VALUES (30000005, STR_TO_DATE('14/02/2021', '%d/%m/%Y'), 0.50, 'CREDIT', 10000003, 20000003);
+INSERT INTO ydzc_pay VALUES (30000006, STR_TO_DATE('20/02/2021', '%d/%m/%Y'), 2.00, 'DEBIT', 10000004, 20000004);
+INSERT INTO ydzc_pay VALUES (30000007, STR_TO_DATE('20/02/2021', '%d/%m/%Y'), 1.50, 'CASH', 10000005, 20000005);
+INSERT INTO ydzc_pay VALUES (30000008, STR_TO_DATE('22/02/2021', '%d/%m/%Y'), 2.50, 'DEBIT', 10000005, 20000005);
+INSERT INTO ydzc_pay VALUES (30000009, STR_TO_DATE('24/02/2021', '%d/%m/%Y'), 2.00, 'CREDIT', 10000004, 20000004);
+-- ydzc_cc
+INSERT INTO ydzc_cc VALUES (30000002, 'Armin');
+INSERT INTO ydzc_cc VALUES (30000003, 'Eren');
+INSERT INTO ydzc_cc VALUES (30000004, 'Eren');
+INSERT INTO ydzc_cc VALUES (30000005, 'Mikasa');
+INSERT INTO ydzc_cc VALUES (30000009, 'Connie');
+-- ydzc_evt
+INSERT INTO ydzc_evt VALUES (21001, 'Ancient Greek Arts', 'E', STR_TO_DATE('01/03/2021', '%d/%m/%Y'), STR_TO_DATE('03/03/2021', '%d/%m/%Y'));
+INSERT INTO ydzc_evt VALUES (21002, 'Van Gogh Works Exhibition', 'E', STR_TO_DATE('01/03/2021', '%d/%m/%Y'), STR_TO_DATE('01/04/2021', '%d/%m/%Y'));
+INSERT INTO ydzc_evt VALUES (21003, 'Tiny Physical Experiment', 'E', STR_TO_DATE('05/03/2021', '%d/%m/%Y'), STR_TO_DATE('25/03/2021', '%d/%m/%Y'));
+INSERT INTO ydzc_evt VALUES (21004, 'Travel Photography', 'E', STR_TO_DATE('15/03/2021', '%d/%m/%Y'), STR_TO_DATE('16/03/2021', '%d/%m/%Y'));
+INSERT INTO ydzc_evt VALUES (21005, 'The Second War', 'E', STR_TO_DATE('20/03/2021', '%d/%m/%Y'), STR_TO_DATE('27/03/2021', '%d/%m/%Y'));
+INSERT INTO ydzc_evt VALUES (21006, 'Quantum Mechanics Future', 'S', STR_TO_DATE('28/03/2021', '%d/%m/%Y'), STR_TO_DATE('31/03/2021', '%d/%m/%Y'));
+INSERT INTO ydzc_evt VALUES (21007, 'Crystal Mechanics Future', 'S', STR_TO_DATE('28/03/2021', '%d/%m/%Y'), STR_TO_DATE('31/03/2021', '%d/%m/%Y'));
+INSERT INTO ydzc_evt VALUES (21008, 'Flow Mechanics Future', 'S', STR_TO_DATE('28/03/2021', '%d/%m/%Y'), STR_TO_DATE('31/03/2021', '%d/%m/%Y'));
+INSERT INTO ydzc_evt VALUES (21009, 'Structural Mechanics Future', 'S', STR_TO_DATE('28/03/2021', '%d/%m/%Y'), STR_TO_DATE('31/03/2021', '%d/%m/%Y'));
+INSERT INTO ydzc_evt VALUES (21010, 'Engineering Mechanics Future', 'S', STR_TO_DATE('28/03/2021', '%d/%m/%Y'), STR_TO_DATE('31/03/2021', '%d/%m/%Y'));
+-- ydzc_exh
+INSERT INTO ydzc_exh VALUES (21001, 2800.00);
+INSERT INTO ydzc_exh VALUES (21002, 5000.00);
+INSERT INTO ydzc_exh VALUES (21003, 2100.50);
+INSERT INTO ydzc_exh VALUES (21004, 2000.00);
+INSERT INTO ydzc_exh VALUES (21005, 1500.00);
+-- ydzc_sem
+INSERT INTO ydzc_sem VALUES (21006, 1500.00);
+INSERT INTO ydzc_sem VALUES (21007, 2000.00);
+INSERT INTO ydzc_sem VALUES (21008, 600.00);
+INSERT INTO ydzc_sem VALUES (21009, 600.00);
+INSERT INTO ydzc_sem VALUES (21010, 450.00);
+-- ydzc_evt_top
+INSERT INTO ydzc_evt_top VALUES (21001, 'Arts');
+INSERT INTO ydzc_evt_top VALUES (21001, 'History');
+INSERT INTO ydzc_evt_top VALUES (21002, 'Arts');
+INSERT INTO ydzc_evt_top VALUES (21002, 'History');
+INSERT INTO ydzc_evt_top VALUES (21003, 'Children');
+INSERT INTO ydzc_evt_top VALUES (21003, 'Science');
+INSERT INTO ydzc_evt_top VALUES (21004, 'Arts');
+INSERT INTO ydzc_evt_top VALUES (21004, 'Travel');
+INSERT INTO ydzc_evt_top VALUES (21005, 'History');
+INSERT INTO ydzc_evt_top VALUES (21006, 'Science');
+INSERT INTO ydzc_evt_top VALUES (21007, 'Science');
+INSERT INTO ydzc_evt_top VALUES (21008, 'Science');
+INSERT INTO ydzc_evt_top VALUES (21009, 'Science');
+INSERT INTO ydzc_evt_top VALUES (21010, 'Science');
+-- ydzc_reg
+INSERT INTO ydzc_reg VALUES (20211001, 21001, 1);
+INSERT INTO ydzc_reg VALUES (20211002, 21001, 2);
+INSERT INTO ydzc_reg VALUES (20211003, 21001, 3);
+INSERT INTO ydzc_reg VALUES (20211004, 21002, 5);
+INSERT INTO ydzc_reg VALUES (20211005, 21002, 7);
+INSERT INTO ydzc_reg VALUES (20211006, 21002, 8);
+INSERT INTO ydzc_reg VALUES (20211007, 21003, 2);
+INSERT INTO ydzc_reg VALUES (20211008, 21003, 9);
+INSERT INTO ydzc_reg VALUES (20211009, 21004, 5);
+INSERT INTO ydzc_reg VALUES (20211010, 21004, 6);
+-- ydzc_invt
+INSERT INTO ydzc_invt VALUES (20216001, 21006, 1);
+INSERT INTO ydzc_invt VALUES (20216002, 21006, 2);
+INSERT INTO ydzc_invt VALUES (20216003, 21007, 3);
+INSERT INTO ydzc_invt VALUES (20216004, 21007, 4);
+INSERT INTO ydzc_invt VALUES (20216005, 21008, 4);
+INSERT INTO ydzc_invt VALUES (20216006, 21009, 5);
+INSERT INTO ydzc_invt VALUES (20216007, 21009, 6);
+INSERT INTO ydzc_invt VALUES (20216008, 21009, 7);
+-- ydzc_spr
+INSERT INTO ydzc_spr VALUES (NULL, 'I');
+INSERT INTO ydzc_spr VALUES (NULL, 'O');
+INSERT INTO ydzc_spr VALUES (NULL, 'I');
+INSERT INTO ydzc_spr VALUES (NULL, 'I');
+INSERT INTO ydzc_spr VALUES (NULL, 'O');
+INSERT INTO ydzc_spr VALUES (NULL, 'O');
+INSERT INTO ydzc_spr VALUES (NULL, 'I');
+INSERT INTO ydzc_spr VALUES (NULL, 'O');
+INSERT INTO ydzc_spr VALUES (NULL, 'O');
+INSERT INTO ydzc_spr VALUES (NULL, 'I');
+-- ydzc_ind
+INSERT INTO ydzc_ind VALUES (1, 'Erwin', 'Smith');
+INSERT INTO ydzc_ind VALUES (3, 'Hange', 'Zoe');
+INSERT INTO ydzc_ind VALUES (4, 'Levi', 'Ackerman');
+INSERT INTO ydzc_ind VALUES (7, 'Kenny', 'Ackerman');
+INSERT INTO ydzc_ind VALUES (10, 'Frieda', 'Reiss');
+-- ydzc_org
+INSERT INTO ydzc_org VALUES (2, 'Google');
+INSERT INTO ydzc_org VALUES (5, 'Facebook');
+INSERT INTO ydzc_org VALUES (6, 'Twitter');
+INSERT INTO ydzc_org VALUES (8, 'Netflix');
+INSERT INTO ydzc_org VALUES (9, 'FIFA');
+-- ydzc_fund
+INSERT INTO ydzc_fund VALUES (500.00, 1, 21006);
+INSERT INTO ydzc_fund VALUES (1000.00, 2, 21006);
+INSERT INTO ydzc_fund VALUES (1200.00, 3, 21007);
+INSERT INTO ydzc_fund VALUES (800.00, 4, 21007);
+INSERT INTO ydzc_fund VALUES (600.00, 4, 21008);
+INSERT INTO ydzc_fund VALUES (250.00, 5, 21009);
+INSERT INTO ydzc_fund VALUES (350.00, 6, 21009);
+INSERT INTO ydzc_fund VALUES (450.00, 7, 21010);
+-- ydzc_rm
+INSERT INTO ydzc_rm VALUES (101, 80);
+INSERT INTO ydzc_rm VALUES (102, 80);
+INSERT INTO ydzc_rm VALUES (103, 80);
+INSERT INTO ydzc_rm VALUES (201, 30);
+INSERT INTO ydzc_rm VALUES (202, 50);
+INSERT INTO ydzc_rm VALUES (301, 20);
+-- ydzc_rmsect
+INSERT INTO ydzc_rmsect VALUES (NULL, STR_TO_DATE('01/04/2021', '%d/%m/%Y'), 'A', 50, 101);
+INSERT INTO ydzc_rmsect VALUES (NULL, STR_TO_DATE('01/04/2021', '%d/%m/%Y'), 'A', 0, 102);
+INSERT INTO ydzc_rmsect VALUES (NULL, STR_TO_DATE('01/04/2021', '%d/%m/%Y'), 'A', 26, 103);
+INSERT INTO ydzc_rmsect VALUES (NULL, STR_TO_DATE('01/04/2021', '%d/%m/%Y'), 'B', 15, 101);
+INSERT INTO ydzc_rmsect VALUES (NULL, STR_TO_DATE('01/04/2021', '%d/%m/%Y'), 'B', 8, 102);
+INSERT INTO ydzc_rmsect VALUES (NULL, STR_TO_DATE('02/04/2021', '%d/%m/%Y'), 'A', 16, 301);
+INSERT INTO ydzc_rmsect VALUES (NULL, STR_TO_DATE('02/04/2021', '%d/%m/%Y'), 'D', 20, 301);
+INSERT INTO ydzc_rmsect VALUES (NULL, STR_TO_DATE('03/04/2021', '%d/%m/%Y'), 'B', 24, 202);
+-- ydzc_resv
+INSERT INTO ydzc_resv VALUES (1, 2);
+INSERT INTO ydzc_resv VALUES (2, 2);
+INSERT INTO ydzc_resv VALUES (3, 2);
+INSERT INTO ydzc_resv VALUES (1, 5);
+INSERT INTO ydzc_resv VALUES (2, 5);
+INSERT INTO ydzc_resv VALUES (5, 6);
+INSERT INTO ydzc_resv VALUES (7, 6);
+INSERT INTO ydzc_resv VALUES (8, 6);
+INSERT INTO ydzc_resv VALUES (9, 6);
---
+++ model.sql
@@ -0,0 +1,210 @@
+CREATE TABLE ydzc_auth (
+    auth_id       INT NOT NULL AUTO_INCREMENT COMMENT 'AUTHOR ID',
+    auth_fname    VARCHAR(20) NOT NULL COMMENT 'AUTHOR FIRST NAME',
+    auth_lname    VARCHAR(20) NOT NULL COMMENT 'AUTHOR LAST NAME',
+    auth_email    VARCHAR(25) NOT NULL COMMENT 'AUTHOR EMAIL',
+    auth_hno      VARCHAR(10) NOT NULL COMMENT 'AUTHOR HOUSE NUMBER',
+    auth_st       VARCHAR(20) NOT NULL COMMENT 'AUTHOR STREET',
+    auth_city     VARCHAR(20) NOT NULL COMMENT 'AUTHOR CITY',
+    auth_state    CHAR(2) NOT NULL COMMENT 'AUTHOR STATE ABBREVIATION',
+    auth_country  CHAR(2) NOT NULL COMMENT 'AUTHOR COUNTRY ABBREVIATION',
+    auth_zip      INT NOT NULL COMMENT 'AUTHOR ZIPCODE',
+	PRIMARY KEY (auth_id)
+);
+
+CREATE TABLE ydzc_bk (
+    bk_isbn   BIGINT NOT NULL COMMENT 'INTERNATION STANDARD BOOK NUMBER',
+    bk_title  VARCHAR(30) NOT NULL COMMENT 'BOOK TITLE',
+	PRIMARY KEY (bk_isbn)
+);
+
+CREATE TABLE ydzc_bk_auth (
+    auth_id  INT NOT NULL,
+    bk_isbn  BIGINT NOT NULL,
+	PRIMARY KEY (auth_id, bk_isbn),
+	FOREIGN KEY (auth_id) REFERENCES ydzc_auth(auth_id),
+	FOREIGN KEY (bk_isbn) REFERENCES ydzc_bk(bk_isbn)
+);
+
+CREATE TABLE ydzc_top (
+    top_name VARCHAR(20) NOT NULL COMMENT 'TOPIC NAME',
+	PRIMARY KEY (top_name)
+);
+
+CREATE TABLE ydzc_bk_top (
+    bk_isbn   BIGINT NOT NULL,
+    top_name  VARCHAR(20) NOT NULL,
+	PRIMARY KEY (bk_isbn, top_name),
+	FOREIGN KEY (bk_isbn) REFERENCES ydzc_bk(bk_isbn),
+	FOREIGN KEY (top_name) REFERENCES ydzc_top(top_name)
+);
+
+CREATE TABLE ydzc_bkcpy (
+    bkcpy_id    INT NOT NULL AUTO_INCREMENT COMMENT 'BOOK COPY ID',
+    bkcpy_stat  CHAR(1) NOT NULL COMMENT 'BOOK COPY STATUS: AVAILABLE(Y) OR UNAVAILABLE(N)',
+    bk_isbn     BIGINT NOT NULL,
+	PRIMARY KEY (bkcpy_id),
+	FOREIGN KEY (bk_isbn) REFERENCES ydzc_bk(bk_isbn),
+	CHECK(bkcpy_stat in ('Y', 'N'))
+);
+
+CREATE TABLE ydzc_cust (
+    cust_id      INT NOT NULL AUTO_INCREMENT COMMENT 'CUSTOMER ID',
+    cust_fname   VARCHAR(20) NOT NULL COMMENT 'CUSTOMER FIRST NAME',
+    cust_lname   VARCHAR(20) NOT NULL COMMENT 'CUSTOMER LAST NAME',
+    cust_phone   BIGINT NOT NULL COMMENT 'CUSTOMER PHONE NUMBER',
+    cust_email   VARCHAR(30) NOT NULL COMMENT 'CUSTOMER EMAIL',
+    cust_idtype  CHAR(1) NOT NULL COMMENT 'CUSTOMER ID TYPE: DRIVER LICENSE(D), PASSPORT(P) OR SSN(S)',
+    cust_idno    BIGINT NOT NULL COMMENT 'CUSTOMER ID NUMBER',
+	PRIMARY KEY (cust_id),
+	CHECK (cust_idtype in ('P', 'S', 'D'))
+);
+
+CREATE TABLE ydzc_rent (
+    rent_id      INT NOT NULL AUTO_INCREMENT COMMENT 'RENTAL ID',
+    rent_stat    CHAR(1) NOT NULL COMMENT 'RENTAL STATUS: BORROWED(B), LATE(L), RETURNED(R)',
+    rent_bordt   DATETIME NOT NULL COMMENT 'BORROW DATE',
+    rent_erntdt  DATETIME NOT NULL COMMENT 'EXPECTED RETURN DATE',
+    rent_arntdt  DATETIME COMMENT 'ACTUAL RETURN DATE',
+    bkcpy_id     INT NOT NULL,
+    cust_id      INT NOT NULL,
+	PRIMARY KEY (rent_id),
+	FOREIGN KEY (bkcpy_id) REFERENCES ydzc_bkcpy(bkcpy_id),
+	FOREIGN KEY (cust_id) REFERENCES ydzc_cust(cust_id),
+	CHECK(rent_stat in ('B', 'L', 'R'))
+);
+
+CREATE TABLE ydzc_invc (
+    invc_id      INT NOT NULL AUTO_INCREMENT COMMENT 'INVOICE ID',
+    invc_date    DATETIME NOT NULL COMMENT 'INVOICE DATE',
+    invc_amount  DECIMAL(6, 2) NOT NULL COMMENT 'INVOICE AMOUNT',
+    rent_id      INT NOT NULL,
+	PRIMARY KEY (invc_id),
+	FOREIGN KEY (rent_id) REFERENCES ydzc_rent(rent_id)
+);
+
+CREATE TABLE ydzc_pay (
+    pay_id      BIGINT NOT NULL AUTO_INCREMENT COMMENT 'PAYMENT ID',
+    pay_date    DATETIME NOT NULL COMMENT 'PAYMENT DATE',
+    pay_amount  DECIMAL(6, 2) NOT NULL COMMENT 'PAYMENT AMOUNT',
+    pay_meth    VARCHAR(8) NOT NULL COMMENT 'PAYMENT METHOD',
+    rent_id     INT NOT NULL,
+	invc_id 	INT NOT NULL,
+	PRIMARY KEY (pay_id),
+	FOREIGN KEY (rent_id) REFERENCES ydzc_rent(rent_id),
+	FOREIGN KEY (invc_id) REFERENCES ydzc_invc(invc_id),
+	CHECK (pay_meth IN ('CREDIT', 'CASH', 'DEBIT', 'PAYPAL'))
+);
+
+CREATE TABLE ydzc_cc (
+    pay_id     BIGINT NOT NULL COMMENT 'PAYMENT ID',
+    paycc_chn  VARCHAR(40) NOT NULL COMMENT 'CREDIT CARD HOLDER NAME',
+	PRIMARY KEY (pay_id),
+	FOREIGN KEY (pay_id) REFERENCES ydzc_pay(pay_id)
+);
+
+CREATE TABLE ydzc_evt (
+    evt_id       INT NOT NULL AUTO_INCREMENT COMMENT 'EVENT ID',
+    evt_name     VARCHAR(50) NOT NULL COMMENT 'EVENT NAME',
+    evt_type     CHAR(1) NOT NULL COMMENT 'EVENT TYPE',
+    evt_startdt  DATETIME NOT NULL COMMENT 'EVENT START DATETIME',
+    evt_stopdt   DATETIME NOT NULL COMMENT 'EVENT STOP DATETIME',
+	PRIMARY KEY (evt_id),
+	CHECK (evt_type IN ('E', 'S'))
+);
+
+CREATE TABLE ydzc_exh (
+    evt_id          INT NOT NULL COMMENT 'EVENT ID',
+    evtexh_expense  DECIMAL(6, 2) NOT NULL COMMENT 'EXHIBITION EXPENSE',
+	PRIMARY KEY (evt_id),
+	FOREIGN KEY (evt_id) REFERENCES ydzc_evt(evt_id)
+);
+
+CREATE TABLE ydzc_sem (
+    evt_id          INT NOT NULL COMMENT 'EVENT ID',
+    evtsem_totfund  DECIMAL(10, 2) NOT NULL COMMENT 'TOTAL FUND AMOUNT',
+	PRIMARY KEY (evt_id),
+	FOREIGN KEY (evt_id) REFERENCES ydzc_evt(evt_id)
+);
+
+CREATE TABLE ydzc_evt_top (
+    evt_id    INT NOT NULL,
+    top_name  VARCHAR(20) NOT NULL,
+	PRIMARY KEY (evt_id, top_name),
+	FOREIGN KEY (evt_id) REFERENCES ydzc_evt(evt_id),
+	FOREIGN KEY (top_name) REFERENCES ydzc_top(top_name)
+);
+
+CREATE TABLE ydzc_reg (
+    reg_id   BIGINT NOT NULL AUTO_INCREMENT COMMENT 'REGISTRATION ID FOR AUTHOR TO JOIN SEMINAR',
+    evt_id   INT NOT NULL,
+    cust_id  INT NOT NULL,
+	PRIMARY KEY (reg_id),
+	FOREIGN KEY (evt_id) REFERENCES ydzc_exh(evt_id),
+	FOREIGN KEY (cust_id) REFERENCES ydzc_cust(cust_id)
+);
+
+CREATE TABLE ydzc_invt (
+    invt_id  INT NOT NULL AUTO_INCREMENT COMMENT 'INVITATION ID FOR CUSTOMER TO ATTEND EXHIBITION',
+    evt_id   INT NOT NULL,
+    auth_id  INT NOT NULL,
+	PRIMARY KEY (invt_id),
+	FOREIGN KEY (evt_id) REFERENCES ydzc_sem(evt_id),
+	FOREIGN KEY (auth_id) REFERENCES ydzc_auth(auth_id)
+);
+
+CREATE TABLE ydzc_spr (
+    spr_id    SMALLINT NOT NULL AUTO_INCREMENT COMMENT 'SPONSOR ID',
+    spr_type  CHAR(1) NOT NULL COMMENT 'SPONSOR TYPE: INDIVIDUAL(I) OR ORGANIZATION(O)',
+	PRIMARY KEY (spr_id),
+	CHECK (spr_type IN ('I', 'O'))
+);
+
+CREATE TABLE ydzc_ind (
+    spr_id        SMALLINT NOT NULL COMMENT 'SPONSOR ID',
+    sprind_fname  VARCHAR(20) NOT NULL COMMENT 'INDIVIDUAL SPONSOR FIRST NAME',
+    sprind_lname  VARCHAR(20) NOT NULL COMMENT 'INDIVIDUAL SPONSOR LAST NAME',
+	PRIMARY KEY (spr_id),
+	FOREIGN KEY (spr_id) REFERENCES ydzc_spr(spr_id)
+);
+
+CREATE TABLE ydzc_org (
+    spr_id       SMALLINT NOT NULL COMMENT 'SPONSOR ID',
+    sprorg_name  VARCHAR(40) NOT NULL COMMENT 'ORGANIZATION SPONSOR NAME',
+	PRIMARY KEY (spr_id),
+	FOREIGN KEY (spr_id) REFERENCES ydzc_spr(spr_id)
+);
+
+CREATE TABLE ydzc_fund (
+    fund_amount  DECIMAL(10, 2) NOT NULL COMMENT 'SPONSOR FUND AMOUNT',
+    spr_id       SMALLINT NOT NULL,
+    evt_id       INT NOT NULL,
+	PRIMARY KEY (spr_id, evt_id),
+	FOREIGN KEY (spr_id) REFERENCES ydzc_spr(spr_id),
+	FOREIGN KEY (evt_id) REFERENCES ydzc_sem(evt_id)
+);
+
+CREATE TABLE ydzc_rm (
+    rm_id   SMALLINT NOT NULL COMMENT 'ROOM ID',
+    rm_cap  SMALLINT NOT NULL COMMENT 'ROOM CAPACITY',
+	PRIMARY KEY (rm_id)
+);
+
+CREATE TABLE ydzc_rmsect (
+    rmsect_id      INT NOT NULL AUTO_INCREMENT COMMENT 'ROOM SECTION ID IDENTIFY DATE AND TIMESLOT',
+    rmsect_dt      DATETIME NOT NULL COMMENT 'ROOM SECTION DATE',
+    rmsect_ts      CHAR(1) NOT NULL COMMENT 'RESERVATION TIMESLOT: 8AM-10AM(A),11AM-1PM(B),1PM-3PM(C),4PM-6PM(D)',
+    rmsect_rmncap  SMALLINT NOT NULL COMMENT 'REMIANING ROOM CAPACITY',
+    rm_id          SMALLINT NOT NULL,
+	PRIMARY KEY (rmsect_id),
+	FOREIGN KEY (rm_id) REFERENCES ydzc_rm(rm_id),
+	CHECK(rmsect_ts in ('A', 'B', 'C', 'D'))
+);
+
+CREATE TABLE ydzc_resv (
+    cust_id    INT NOT NULL,
+    rmsect_id  INT NOT NULL,
+	PRIMARY KEY (cust_id, rmsect_id),
+	FOREIGN KEY (cust_id) REFERENCES ydzc_cust(cust_id),
+	FOREIGN KEY (rmsect_id) REFERENCES ydzc_rmsect(rmsect_id)
+);
